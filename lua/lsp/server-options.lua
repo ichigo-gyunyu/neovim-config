497e426da777a26b2496bb615bca5ccfb26e2f4e
ichigo--gyuunyuu <15829692+ichigo-gyuunyuu@users.noreply.github.com>
2022-01-04 18:48:17 +0530

    lsp

---
+++ lua/lsp/server-options.lua
@@ -0,0 +1,20 @@
+local M = {}
+
+M.sumneko_lua = {
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+			workspace = {
+				library = {
+					-- Make the server aware of Neovim runtime files
+					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+					[vim.fn.stdpath("config") .. "/lua"] = true,
+				},
+			},
+		},
+	},
+}
+
+return M
