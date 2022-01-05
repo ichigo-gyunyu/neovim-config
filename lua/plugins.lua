ee90b18f9a80144edb70a61f711a0aab192191c4
ichigo--gyuunyuu <15829692+ichigo-gyuunyuu@users.noreply.github.com>
2022-01-05 11:39:46 +0530

    lualine

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -83,6 +83,9 @@ return packer.startup(function(use)
     use "akinsho/bufferline.nvim"
     use "moll/vim-bbye"
 
+    -- Statusline
+    use "nvim-lualine/lualine.nvim"
+
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if PACKER_BOOTSTRAP then
