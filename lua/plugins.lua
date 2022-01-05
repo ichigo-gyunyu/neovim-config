c18254cb7b5fe6aea4dd51f24891374cb3b7bc78
ichigo--gyuunyuu <15829692+ichigo-gyuunyuu@users.noreply.github.com>
2022-01-05 12:02:18 +0530

    impatient

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -89,6 +89,9 @@ return packer.startup(function(use)
     -- Terminal
     use "akinsho/toggleterm.nvim"
 
+    -- Impatient
+    use "lewis6991/impatient.nvim"
+
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if PACKER_BOOTSTRAP then
