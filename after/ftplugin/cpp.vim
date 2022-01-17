ee5c29fa6ee6e06a6b1499ccb32314f019224507
ichigo--gyuunyuu <15829692+ichigo-gyuunyuu@users.noreply.github.com>
2022-01-17 15:39:01 +0530

    toggleterm stuff

--- after/ftplugin/cpp.vim
+++ after/ftplugin/cpp.vim
@@ -1,5 +1,7 @@
-if !empty(expand(glob("./in")))
-    nnoremap <buffer> <leader>r :!g++ -std=c++20 -O2 -Wall -g -fsanitize=address -Dichigo_runtime -o out % && ./out < in<CR>
-else
-    nnoremap <buffer> <leader>r :!g++ -std=c++20 -O2 -Wall -g -fsanitize=address -Dichigo_runtime -o out % && ./out <CR>
-endif
+" if !empty(expand(glob("./in")))
+"     nnoremap <buffer> <leader>r :!g++ -std=c++20 -O2 -Wall -g -fsanitize=address -Dichigo_runtime -o out % && ./out < in<CR>
+" else
+"     nnoremap <buffer> <leader>r :!g++ -std=c++20 -O2 -Wall -g -fsanitize=address -Dichigo_runtime -o out % && ./out <CR>
+" endif
+
+nnoremap <buffer> <leader>r :TermExec cmd=make<CR>
--- lua/plugconfig/toggleterm.lua
+++ lua/plugconfig/toggleterm.lua
@@ -6,7 +6,7 @@ end
 toggleterm.setup {
     size = function(term)
         if term.direction == "horizontal" then
-            return 15
+            return 25
         elseif term.direction == "vertical" then
             return vim.o.columns * 0.4
         else
@@ -22,7 +22,7 @@ toggleterm.setup {
     start_in_insert = true,
     insert_mappings = true, -- whether or not the open mapping applies in insert mode
     persist_size = true,
-    direction = "horizontal",
+    direction = "vertical",
     close_on_exit = true, -- close the terminal window when the process exits
     shell = vim.o.shell, -- change the default shell
     -- This field is only relevant if direction is set to 'float'
