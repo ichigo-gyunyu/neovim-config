1ed67a6c68831177cc512ce2f27a13acdbcfe13d
ichigo--gyuunyuu <15829692+ichigo-gyuunyuu@users.noreply.github.com>
2022-03-30 23:07:31 +0530

    term

--- lua/plugconfig/toggleterm.lua
+++ lua/plugconfig/toggleterm.lua
@@ -17,7 +17,7 @@ toggleterm.setup {
     -- on_open = fun(t: Terminal), -- function to run when the terminal opens
     hide_numbers = true, -- hide the number column in toggleterm buffers
     shade_filetypes = {},
-    shade_terminals = true,
+    shade_terminals = false,
     shading_factor = 1, -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
     start_in_insert = true,
     insert_mappings = true, -- whether or not the open mapping applies in insert mode
