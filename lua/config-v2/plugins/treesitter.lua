0fb51c941552238f6514bea9cadfb83f8a51d61f
ichigo-gyunyu <15829692+ichigo-gyunyu@users.noreply.github.com>
2023-11-06 20:56:12 +0530

    fix treesitter not automatically loading on startup

--- lua/config-v2/plugins/treesitter.lua
+++ lua/config-v2/plugins/treesitter.lua
@@ -2,10 +2,10 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	version = false,
 	build = ":TSUpdate",
-	cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
 	opts = {
 		highlight = { enable = true },
 		indent = { enable = true },
 		incremental_selection = { enable = true },
 	},
+	main = "nvim-treesitter.configs",
 }
