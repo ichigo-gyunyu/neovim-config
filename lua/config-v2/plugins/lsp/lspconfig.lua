3d2586b90ccc52680e58dd590e97fd872b9ea50a
ichigo-gyunyu <15829692+ichigo-gyunyu@users.noreply.github.com>
2024-09-27 23:04:03 +0530

    Add omnisharp config

--- lua/config-v2/plugins/lsp/lspconfig.lua
+++ lua/config-v2/plugins/lsp/lspconfig.lua
@@ -28,6 +28,14 @@ return {
         },
       },
 
+      omnisharp = {
+        enable_roslyn_analysers = true,
+        enable_import_completion = true,
+        organize_imports_on_format = true,
+        enable_decompilation_support = true,
+        filetypes = { "cs", "vb", "csproj", "sln", "slnx", "props", "csx", "targets" },
+      },
+
       rust_analyzer = {},
     },
   },
