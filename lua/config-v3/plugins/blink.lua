8cb7597a2e4268bc793624428bdbf3ce64f8a7ca
ichigo-gyunyu <15829692+ichigo-gyunyu@users.noreply.github.com>
2025-09-25 18:50:17 +0530

    use super-tab

--- lua/config-v3/plugins/blink.lua
+++ lua/config-v3/plugins/blink.lua
@@ -14,7 +14,7 @@ return {
     -- C-k: Toggle signature help (if signature.enabled = true)
     --
     -- See :h blink-cmp-config-keymap for defining your own keymap
-    keymap = { preset = "default" },
+    keymap = { preset = "super-tab" },
 
     signature = { enabled = true },
 
